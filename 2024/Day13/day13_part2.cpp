5a036e1cd40dcb07b6349fe4f602cb9acf669fd2
KhalidAlsantali <K.alsantali@gmail.com>
2024-12-13 19:39:29 +0300

    clean up

--- 2024/Day13/day13_part2.cpp
+++ 2024/Day13/day13_part2.cpp
@@ -8,18 +8,17 @@
 using namespace std;
 
 vector<vector<int64_t>> mulMat(vector<vector<int64_t>>& m1, vector<vector<int64_t>>& m2){
-    vector<vector<int64_t>> result;
     int r1 = m1.size();
     int c1 = m1[0].size();
     int r2 = m2.size();
     int c2 = m2[0].size();
 
+    vector<vector<int64_t>> result(r1, vector<int64_t>(c2, 0));
+
     if (c1 != r2) {
         cout << "Invalid Input" << endl;
         exit(EXIT_FAILURE);
     }
-
-    result.resize(r1, vector<int64_t>(c2, 0)); 
   
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c2; j++) {
