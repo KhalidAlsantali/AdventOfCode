b33792571cecf7a50ea82cacc7fa5e721831e3db
KhalidAlsantali <K.alsantali@gmail.com>
2024-12-19 19:10:51 +0300

    day 19 part 2

--- 2024/Day19/day19.cpp
+++ 2024/Day19/day19.cpp
@@ -5,11 +5,11 @@
 #include <string>
 #include <algorithm>
 #include <unordered_map>
-
 using namespace std;
 
 vector<string> patterns;
 unordered_map<string, bool> memo;
+unordered_map<string, int64_t> memo_ways;
 
 bool findPattern(string pattern, vector<string>& patterns){
     if(memo.count(pattern) > 0){
@@ -24,7 +24,7 @@ bool isDesignPossible(const string& design) {
     if(design.empty()) {
         return true;
     }
-    
+   
     for(int len = 1; len <= design.length(); len++) {
         string prefix = design.substr(0, len);
         if(findPattern(prefix, patterns)) {
@@ -33,8 +33,27 @@ bool isDesignPossible(const string& design) {
             }
         }
     }
-    
+   
     return false;
+}
+
+int64_t countWays(const string& design) {
+    if(design.empty()) {
+        return 1;
+    }
+    if(memo_ways.count(design) > 0) {
+        return memo_ways[design];
+    }
+   
+    int64_t ways = 0;
+    for(int len = 1; len <= design.length(); len++) {
+        string prefix = design.substr(0, len);
+        if(findPattern(prefix, patterns)) {
+            ways += countWays(design.substr(len));
+        }
+    }
+    memo_ways[design] = ways;
+    return ways;
 }
 
 int main() {
@@ -43,37 +62,35 @@ int main() {
         cerr << "Error: Unable to open file." << endl;
         return 1;
     }
-
     string line;
     vector<string> designs;
-
     if (getline(inputFile, line)) {
         stringstream ss(line);
         string item;
         while (getline(ss, item, ',')) {
             item.erase(0, item.find_first_not_of(' '));
+            item.erase(item.find_last_not_of(' ') + 1);
             patterns.push_back(item);
         }
     }
     getline(inputFile, line);
-
     while (getline(inputFile, line)) {
         designs.push_back(line);
     }
-
     inputFile.close();
-
+    
     sort(patterns.begin(), patterns.end());
-
     int sum = 0;
+    int64_t sum2 = 0;
     for(const auto& design : designs){
-        cout << design;
         if(isDesignPossible(design)){
             sum++;
-            continue;
         }
+        memo_ways.clear(); 
+        int64_t ways = countWays(design);
+        sum2 += ways;
     }
-
-    cout << "Part one answer: " << sum;
+    cout << "Part one answer: " << sum << endl;
+    cout << "Part two answer: " << sum2 << endl;
     return 0;
 }
