591c126f380bb8932f7a871beb6ff9c8af22e40c
KhalidAlsantali <K.alsantali@gmail.com>
2024-12-27 13:55:05 +0300

    day 3 part 1

---
+++ 2017/Day03/day3_part1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream inputFile("input.txt");
+    if (!inputFile) {
+        std::cerr << "Unable to open file input.txt";
+        return 1;
+    }
+
+    std::string line;
+    std::getline(inputFile, line);
+    inputFile.close();
+
+    int input = std::stoi(line);
+
+    int layer = 0;
+    while ((2 * layer + 1) * (2 * layer + 1) < input) {
+        layer++;
+    }
+
+    int sideLength = 2 * layer + 1;
+    int maxInLayer = sideLength * sideLength;
+
+    int stepsBackFromMax = maxInLayer - input;
+
+    int sideOffset = stepsBackFromMax % (sideLength - 1);
+
+    int distance = layer + abs(sideOffset - layer);
+
+    std::cout << "Manhattan distance: " << distance << std::endl;
+
+    return 0;
+}
