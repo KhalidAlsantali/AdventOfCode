c23bfbc692fd75e22705413dccebb352f828dac1
KhalidAlsantali <K.alsantali@gmail.com>
2025-01-05 17:13:15 +0300

    day 17 part 2

--- 2017/Day17/day17.cpp
+++ 2017/Day17/day17.cpp
@@ -35,6 +35,17 @@ int main() {
     vector<int>::iterator it = find(buffer.begin(), buffer.end(), 2017);
     int result = (it != buffer.end() && next(it) != buffer.end()) ? *next(it) : -1;
 
+    int result2 = 0;
+    index = 0;
+    counter = 1;
+    while(counter <= 50000000){
+        index = (index + step) % counter;
+        ++index;
+        if(index == 1) result2 = counter;
+        ++counter;
+    }
+
     cout << "Part one answer: " << result << endl;
+    cout << "Part two answer: " << result2 << endl;
     return 0;
 }
